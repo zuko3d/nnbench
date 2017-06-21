bcb4ba34c0001840b2f0e120e88a3cf8a2e874bc
zuko3d <zuko3d@gmail.com>
2017-06-21 13:02:06 +0300

    Added cntk

---
+++ cntk_cpu.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+import numpy as np
+import time
+
+print("Generating dataset...")
+X = np.random.uniform(size = (50000, 100)).astype(np.float32)
+y = X
+
+# =====================================================
+
+print("Preparing...")
+ts = time.clock()
+
+import cntk
+from cntk.layers import *
+
+print(cntk.try_set_default_device(cntk.cpu()))
+
+x = cntk.input_variable(100, np.float32)
+target = cntk.input_variable(100, np.float32)
+
+model = Sequential ([
+    Dense(400, activation=tanh),
+    Dense(200, activation=tanh),
+    Dense(200, activation=tanh),
+    Dense(200, activation=tanh),
+    Dense(100, activation=tanh)
+]) (x)
+
+loss = cntk.squared_error(model, target)
+learner = cntk.sgd(model.parameters, cntk.learning_rate_schedule(0.01, cntk.UnitType.minibatch))
+
+trainer = cntk.Trainer(model, (loss, loss), [learner])
+
+print("Time elapsed: ", time.clock() - ts)
+
+# ===================================================
+
+print("Calculating...")
+ts = time.clock()
+
+for _ in range(200):
+	trainer.train_minibatch({x : X, target : y})
+
+print("Time elapsed: ", time.clock() - ts)
